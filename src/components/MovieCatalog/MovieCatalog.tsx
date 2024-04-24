c9b770745a76cf235fdba5e2dd3065ddd017e0a9
largoui <largou.imane.mpsi@gmail.com>
2024-04-24 21:24:26 +0200

    feat: infinite scroll on movies

--- src/components/MovieCatalog/MovieCatalog.tsx
+++ src/components/MovieCatalog/MovieCatalog.tsx
@@ -1,12 +1,48 @@
 import styles from "./MovieCatalog.module.css";
 
 import GenreList from "../GenreList/GenreList";
+import MoviePreview from "../MoviePreview/MoviePreview";
+import { useInfiniteQuery } from "react-query";
+import { getMovies } from "../../services/movieService";
+import { useEffect } from "react";
 
 export const MovieCatalog = () => {
+  const {
+    fetchNextPage,
+    hasNextPage,
+    data,
+    isLoading
+  } = useInfiniteQuery({
+    queryKey: ["getPopularMovies"],
+    queryFn: ({ pageParam = 1 }) => getMovies(pageParam),
+    getNextPageParam: (_lastPage, allPages) => {return allPages.length + 1},
+  });
+
+  const moviesList = data?.pages.flat() || []
+  useEffect(()=>{
+    const handleScroll = () => {
+      if (document.body.offsetHeight <= window.innerHeight + window.scrollY && hasNextPage){
+        fetchNextPage()
+      }
+    }
+
+    window.addEventListener("scroll",handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll);
+  },[moviesList])
+
   return (
     <>
-      <GenreList />
-      <div className={styles.layout}></div>
+    {isLoading && <div>Chargement...</div>}
+    {!isLoading && 
+      <>
+        <GenreList />
+        <div className={styles.layout}>
+          {moviesList.map((movie)=>(
+            <MoviePreview movie={movie}/>
+          ))}
+        </div>
+      </>
+    } 
     </>
   );
 };
--- src/components/MoviePreview/MoviePreview.tsx
+++ src/components/MoviePreview/MoviePreview.tsx
@@ -1,11 +1,22 @@
+import { POSTER_URL_PREFIX } from "../../App";
+import { Movie } from "../../models";
 import styles from "./MoviePreview.module.css";
 
-const MoviePreview = () => {
+const MoviePreview = ({movie}:{movie: Movie}) => {
   const goToMovieDetails = () => {
-    // A DEFINIR EXPERT
+    window.location.href = `/movie/${movie.id}`;
   };
-
-  return <div className={styles.movieCard} onClick={goToMovieDetails}></div>;
+  const vote_adjusted = Math.floor(movie.vote_average*5/10)
+  return <div className={styles.movieCard} onClick={goToMovieDetails}>
+    <img src={POSTER_URL_PREFIX + movie.poster_path} height={200} width={150} style={{borderTopLeftRadius:"5px",borderTopRightRadius:"5px"}}/>
+    <div>{movie.title}</div>
+    <div className={styles.vote}>
+      {[...Array(5)].map((_, index) => (
+        <div style={{color:`${vote_adjusted<index+1 ? "white" : "yellow"}`
+        }}>★</div>
+      ))}
+    </div>
+  </div>;
 };
 
 export default MoviePreview;
